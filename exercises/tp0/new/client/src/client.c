8fb95a4174e82ac05e9f9c9d9ba1b36245974527
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-09-03 17:06:23 -0300

    init 2c

---
+++ exercises/tp0/new/client/src/client.c
@@ -0,0 +1,85 @@
+#include "client.h"
+
+int main(void)
+{
+	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
+	int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+
+	t_log* logger;
+	t_config* config;
+
+	logger = iniciar_logger();
+
+	// Usando el logger creado previamente
+	// Escribi: "Hola! Soy un log"
+
+	config = iniciar_config();
+
+	// Usando el config creado previamente
+	// Lee las variables de IP, Puerto y Valor
+
+	//Loggear valor de config
+
+	leer_consola(logger);
+
+
+	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+
+	// Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo porque lo necesitaremos para lo que sigue.
+
+	// Creamos una conexión hacia el servidor
+	conexion = crear_conexion(ip, puerto);
+
+	//enviar CLAVE al servirdor
+
+	paquete(conexion);
+
+	terminar_programa(conexion, logger, config);
+
+	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
+	// Proximamente
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger;
+
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config;
+
+	return nuevo_config;
+}
+
+void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	//El primero te lo dejo de yapa
+	leido = readline(">");
+
+	// Acá la idea es que imprimas por el log lo que recibis de la consola.
+
+
+}
+
+void paquete(int conexion)
+{
+	//Ahora toca lo divertido!
+
+	char* leido;
+	t_paquete* paquete;
+
+
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	//Y por ultimo, para cerrar, hay que liberar lo que utilizamos (conexion, log y config) con las funciones de las commons y del TP mencionadas en el enunciado
+}
